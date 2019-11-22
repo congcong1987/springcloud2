f2017333292236ed85ebedbd9ff9adb7af5a39ac
congcong1987 <897478281@qq.com>
2019-11-22 16:10:37 +0800

    springcloud jdbc服务端配置

---
+++ springcloud-configer-server-jdbc/src/main/java/com/itcast/springcloud_configer_server_jdbc/App.java
@@ -0,0 +1,13 @@
+package com.itcast.springcloud_configer_server_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
