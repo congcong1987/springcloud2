e69e6f188b0aa3708bbf6039824d813186fd6933
congcong1987 <897478281@qq.com>
2019-11-22 16:12:32 +0800

    springcloud config jdbc 客户端

---
+++ springcloud-config-client-jdbc/src/main/java/com/itcast/springcloud_config_client_jdbc/App.java
@@ -0,0 +1,13 @@
+package com.itcast.springcloud_config_client_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
